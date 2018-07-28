cb03023324608d9d72860b8f4a98aed8bf2e3fbc
Amala George <ammu@Amalas-MacBook-Pro.local>
2018-07-28 16:29:38 +0530

    add exercise 2

---
+++ ex2.py
@@ -0,0 +1,12 @@
+print "i could have code like this" #comments after this are ignored
+print "i will now count my chickens"
+print "hens" , 25+30/6
+print "roosters" , 100-25*3%4
+print "now i will count the eggs"
+print 3+2+1-5+4%2-1/4+6
+print "is this true that 3+2 < 5-7?"
+print "3+2 < 5-7"
+print "what is 3+2?" , 3+2
+print "is it greater",5>-2
+print "is it greatr or equal",5>=-2
+print "is it less or equal", 5<= -2
